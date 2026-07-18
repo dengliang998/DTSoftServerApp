b064a50d13ac5f11d0ac3e19e7872766aa514396
dengliang <dengliang7080@163.com>
2026-07-18 14:32:45 +0800

    移除sql

--- DTSoftServerApp/Migrations/20260711_AddMicroAppFormColumns.sql
+++
@@ -1,7 +0,0 @@
--- 为微应用配置表补齐数据表单每行列数字段。
-
-IF COL_LENGTH('dbo.sys_microappconfig', 'FormColumns') IS NULL
-BEGIN
-    ALTER TABLE [dbo].[sys_microappconfig]
-    ADD [FormColumns] INT NOT NULL CONSTRAINT [DF_sys_microappconfig_FormColumns] DEFAULT 1;
-END
--- DTSoftServerApp/Migrations/20260711_AddMicroAppQueryColumns.sql
+++
@@ -1,7 +0,0 @@
--- 为微应用配置表补齐搜索字段每行列数字段。
-
-IF COL_LENGTH('dbo.sys_microappconfig', 'QueryColumns') IS NULL
-BEGIN
-    ALTER TABLE [dbo].[sys_microappconfig]
-    ADD [QueryColumns] INT NOT NULL CONSTRAINT [DF_sys_microappconfig_QueryColumns] DEFAULT 1;
-END
